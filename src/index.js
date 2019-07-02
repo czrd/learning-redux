f7af745b84272fef516a4205c1374c2bd83402f0
Takeshi Itami <tksh.itm@gmail.com>
2019-07-02 22:37:32 +0900

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import { createPost } from './actions';
+import { createPost, editPost, setFilter } from './actions';
 import appReducer, { postsReducer } from './reducers';
 
 let state = appReducer(undefined, { type: 'INIT_ACTION' });
 console.log('initial state:', state);
+
+state = appReducer(state, createPost('Dan', 'New post'));
+console.log('state after createPost:', state);
+
+state = appReducer(state, editPost(0, 'edited post'));
+console.log('state after editPost:', state);
+
+state = appReducer(state, setFilter('none'));
+console.log('state after setFilter:', state);
