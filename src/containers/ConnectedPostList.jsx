453d3db87ced02374357880de4884f872e231700
Takeshi Itami <tksh.itm@gmail.com>
2019-07-02 23:43:41 +0900

    create ConnectPostList with react-redux

--- src/containers/ConnectedPostList.jsx
+++ src/containers/ConnectedPostList.jsx
@@ -1,26 +1,7 @@
-import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PostList from '../components/PostList';
 
-class ConnectedPostList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = props.store.getState();
-  }
-
-  componentDidMount() {
-    const { store } = this.props;
-    this.unsubscribe = store.subscribe(() =>
-      this.setState({ ...store.getState() })
-    );
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    return <PostList {...this.state} />;
-  }
-}
+const mapStateToProps = state => ({ posts: state.posts });
+const ConnectedPostList = connect(mapStateToProps)(PostList);
 
 export default ConnectedPostList;
