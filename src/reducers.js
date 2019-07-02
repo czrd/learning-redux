20cb068c61211c8a1aae7ddcb0d0d96b3301754d
Takeshi Itami <tksh.itm@gmail.com>
2019-07-02 22:26:31 +0900

    add reducers.js

---
+++ src/reducers.js
@@ -0,0 +1,36 @@
+import { CREATE_POST, EDIT_POST, SET_FILTER } from './actionTypes';
+import { combineReducers } from 'redux';
+
+function postsReducer(state = [], action) {
+  switch (action.type) {
+    case CREATE_POST: {
+      const { type, ...post } = action;
+      return [...state, post];
+    }
+    case EDIT_POST: {
+      const { type, ...newPost } = action;
+      return state.map((post, i) =>
+        post.id === i ? { ...post, ...newPost } : post
+      );
+    }
+    default:
+      return state;
+  }
+}
+
+function filterReducer(state = 'all', action) {
+  switch (action.type) {
+    case SET_FILTER: {
+      return action.filter;
+    }
+    default:
+      return state;
+  }
+}
+
+const appReducer = combineReducers({
+  posts: postsReducer,
+  filter: filterReducer
+});
+
+export default appReducer;
